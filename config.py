1c8a769fd6f8199aac35615f83712a1785e4f28c
james23253 <149482544+james23253@users.noreply.github.com>
2026-02-18 14:46:35 +0300

    db url fix

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    _database_url = os.environ.get('DATABASE_POSTGRES_URL', '').replace(
+    _database_url = os.environ.get('DATABASE_URL', '').replace(
         'postgres://', 'postgresql://')
     if _database_url:
         SQLALCHEMY_DATABASE_URI = _database_url
